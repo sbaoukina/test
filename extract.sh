9f27dc18bb5747cd0350d8c2b2f46086f7bcb5f9
rozmanov <dmitri.rozmanov@ucalgary.ca>
2015-10-06 15:27:43 -0600

    Work for Svetlana outlined.

--- extract.sh
+++ extract.sh
@@ -1,1 +1,9 @@
-my script for Dmitri ... ha-ha
+#! /bin/bash
+#------------------------------------
+# my script for Dmitri ... ha-ha
+#------------------------------------
+
+# Svetlana by tomorrow you should implement an new useful method of extracting unique
+# characters from a binary stream....
+
+
